605e8d40ba41bf23d5525bd70755257b2b25824c
mikeunge <mikeunge@protonmail.com>
2020-04-30 15:42:16 +0200

    Created new class (Job), outsourced Programm.start() functionallity, added settings into own settings[] dict, ...

--- run_forever_proc.py
+++ run_forever_proc.py
@@ -14,168 +14,168 @@ from pathlib import Path
 
 
 def readJson(path):
-    """
-    Read the passed json file and return the content (dict).
-    """
-    # Read the json file and return the data.
-    if not os.path.exists(path):
-        # File does not exist.
-        logger.error(f"File does not exist, please make sure the path is correct and the file exists.. [{path}]")
-        return False
-    # Check if the file is empty.
-    if os.stat(path).st_size == 0:
-        # The file is empty..
-        logger.error(f"File is empty, please check the content or given file.. [{path}]")
-        return False
-    try:
-        # Open the file and read the content to data.
-        with open(path, "r") as file:
-            data = json.load(file)
-    except IOError as io:
-        logger.error(f"File is currently busy, please close the json file and try it again. [{path}]")
-        return False
-    except Exception as ex:
-        logger.error(f"Something unexpected happened, please try again.\nError: {ex}", 1)
-        return False
-    return data
+	"""
+	Read the passed json file and return the content (dict).
+	"""
+	data = False
+	if not os.path.exists(path):
+		# File does not exist.
+		logger.error(f"File does not exist, please make sure the path is correct and the file exists.. [{path}]")
+	else:
+		# Check if the file is empty.
+		if os.stat(path).st_size == 0:
+			# The file is empty..
+			logger.error(f"File is empty, please check the content or given file.. [{path}]")
+		else:
+			try:
+				# Open the file and read the content to data.
+				with open(path, "r") as file:
+					data = json.load(file)
+			except IOError as io:
+				logger.error(f"File is currently busy, please close the json file and try it again. [{path}]")
+			except Exception as ex:
+				logger.error(f"Something unexpected happened, please try again.\nError: {ex}", 1)
+	return data
 
 
 def setup_logger(level, path):
-    # Change the logging format below..
-    log_format = "%(asctime)s - %(levelname)s - %(message)s"
-    try:
-        # Check the different cases, default: DEBUG
-        if level.upper() == "DEBUG":
-            logging.basicConfig(filename=path, level=logging.DEBUG, format=log_format)
-        elif level.upper() == "INFO":
-            logging.basicConfig(filename=path, level=logging.INFO, format=log_format)
-        elif level.upper() == "WARNING":
-            logging.basicConfig(filename=path, level=logging.WARNING, format=log_format)
-        elif level.upper() == "ERROR":
-            logging.basicConfig(filename=path, level=logging.ERROR, format=log_format)
-        else:
-            logging.basicConfig(filename=path, level=logging.DEBUG, format=log_format)
-        return logging.getLogger()
-    except Exception as ex:
-        print(f"Could not setup the logger, exiting the program.\nError: {ex}")
-        sys.exit(1)
+	# Change the logging format below..
+	log_format = "%(asctime)s - %(levelname)s - %(message)s"
+	try:
+		# Check the different cases, default: DEBUG
+		if level.upper() == "DEBUG":
+			logging.basicConfig(filename=path, level=logging.DEBUG, format=log_format)
+		elif level.upper() == "INFO":
+			logging.basicConfig(filename=path, level=logging.INFO, format=log_format)
+		elif level.upper() == "WARNING":
+			logging.basicConfig(filename=path, level=logging.WARNING, format=log_format)
+		elif level.upper() == "ERROR":
+			logging.basicConfig(filename=path, level=logging.ERROR, format=log_format)
+		else:
+			logging.basicConfig(filename=path, level=logging.DEBUG, format=log_format)
+		return logging.getLogger()
+	except Exception as ex:
+		print(f"Could not setup the logger, exiting the program.\nError: {ex}")
+		sys.exit(1)
 
 
-class Service:
-    def isUp(self, daemon):
-        """
-        Check if the service/daemon is running or not.
-        """
-        cmd = f"forever list | grep {daemon} >> /dev/null"  # Command to execute.
-        out = os.system(cmd)
-        ERR = False
-        # Check the commands output.
-        if out == 0:
-            msg = f"Daemon [{daemon}] is running"
-            is_up =  True
-        elif out == 256:
-            msg = f"Daemon [{daemon}] is not running"
-            is_up = False
-        else:
-            ERR = True
-            msg = "Something went wrong.."
-            is_up = False
-        # Try if logging is possible.. only used for tests.
-        try:
-            logger.debug(f"Command: {cmd}, out={out}")
-            if ERR:
-                logger.error(msg)
-            else:
-                logger.info(msg)
-        finally:
-            return is_up
+class Job:
+	def __init__(self, max_jobs):
+		MAX_JOBS = max_jobs
+
+
+	def getRegisteredJobs(self, data):
+		"""
+		Destruct the passed data to get all the registered jobs.
+		Loop over the 'jobs' DOM and extract all registered job ids, the numbers are trivial for
+			the controller to function.
+		:param self: [INT] self.MAX_JOBS
+		:param data: [DICT] dict of all jobs in settings.json
+		:return [ARRAY]: job id's of registered jobs eg. [1,2,3,4,...]
+						 if this fails, reutrns [BOOL] -> False
+		"""
+		i = 0
+		reg_jobs = []
+		for job_id in data['jobs']:
+			i += 1
+			reg_jobs.append(job_id)
+			if i == self.MAX_JOBS:
+				break
+		# Return array of registered jobs.
+		return reg_jobs
+
+	
+	def jobController(self, data):
+		"""
+		Load the requested job from the passed data dict.
+		:param self: 
+		:param data: [DICT] dict of all jobs in settings.json
+		:param job_id: [INT] 
+		"""
+		err = False
+		job_ids = getRegisteredJobs(data)	# Get all registered jobs (ARRAY).
+	
+		if err:
+			return False
+		return True
+
+
+	def isUp(self, job):
+		"""
+		Check if the job is running or not.
+		"""
+		cmd = f"forever list | grep {job} >> /dev/null"  # Command to execute.
+		out = os.system(cmd)
+		# Check the commands output.
+		if out == 0:
+			logger.info(f"Daemon [{job}] is running")
+			is_up =  True
+		elif out == 256:
+			logger.info(f"Daemon [{job}] is not running")
+			is_up = False
+		else:
+			logger.error("Something went wrong..")
+			is_up = False
+		return is_up
+
 
 
 class Program:
-    def __init__(self, settings, home_dir, max_jobs):
-        self.SETTINGS = settings
-        self.HOME = home_dir
-        self.MAX_JOBS = max_jobs
+	def __init__(self, settings, max_jobs, home_dir):
+		self.SETTINGS = settings
+		self.HOME = home_dir
+		self.MAX_JOBS = max_jobs
 
 
-    def dataDestruct(self, data):
-        """
-        Destruct the passed data / get all the registered jobs.
-        """
-        i = 0
-        reg_jobs = []
-        for job_id in data['jobs']:
-            i += 1
-            reg_jobs.append(job_id)
-            if i == self.MAX_JOBS:
-                break
-        # Return array of registered jobs.
-        return reg_jobs
+	def start(self):
+		"""
+		Start the program and run the workflow(s).
+		"""
+		# Read the settings file.
+		data = readJson(self.SETTINGS)
+		if type(data) == dict:
+			job = Job(self.MAX_JOBS)
+			job.jobController(data)
 
-
-    def loadJob(self, data, job_id):
-        """
-        Load the requested job from the data dict.
-        Return the formatted job data.
-        """
-        
-        pass
-
-
-    def start(self):
-        # Read the settings file.
-        # TODO: Outsource in new function...
-        data = readJson(self.SETTINGS)
-        if data == False:
-            # Something went wrong while reading the json file...
-            logger.warning("Something went wrong, please check the logs...")
-            sys.exit(1)
-        elif type(data) == dict:    # Check if returned data is a dictonary.
-            dest_data = Program.dataDestruct(self, data)
-            for job in dest_data:
-                print(job)
-            # DEBUG
-            sys.exit(0)
-        else:
-            # Data is corrupt/not valid json formatted..
-            logger.error(f"Data is not a valid json format, please check your file.. [{self.SETTINGS}]")
-            sys.exit(1)
-        service = Service()
-        if service.isUp("bot.js") == True:
-            print("Done")
-        else:
-            print("Error")
+			# DEBUG
+			sys.exit(0)
+		else:
+			# Data is corrupt/not valid json formatted..
+			logger.error(f"Data is not a valid format or corrupt, please check logs for more information..")
+			sys.exit(1)
 
 
 if __name__ == '__main__':
-    # Toggle DEBUG mode.
-    DEBUG = True
+	# Toggle DEBUG mode.
+	DEBUG = True
 
-    # Get the current users home directory.
-    HOME_DIR = str(Path.home())
+	# Get the current users home directory.
+	HOME_DIR = str(Path.home())
 
-    # Define the maximal number of jobs to load.
-    MAX_JOBS = 10
+	# Check if debug mode is active or not.
+	if DEBUG == True:
+		settings = {
+			"LOG_LEVEL": "DEBUG",
+			"LOGGING_PATH": "./forever_proc.log",
+			"SETTINGS_PATH": "./settings.json",
+			"MAX_JOBS": 10
+		}
+	else:
+		settings = {
+			"LOG_LEVEL": "INFO",
+			"LOGGING_PATH": f"{HOME_DIR}/forever_proc.log",
+			"SETTINGS_PATH": f"{HOME_DIR}/.config/forever_proc/settings.json",
+			"MAX_JOBS": 5
+		}
 
-    # Check if debug mode is active or not.
-    # Debugging mode changes paths and logging levels..
-    if DEBUG == True:
-        LOG_LEVEL = "DEBUG"
-        LOGGING_PATH = "./forever_proc.log"
-        SETTINGS_PATH = "./settings.json"
-    else:
-        LOG_LEVEL = "INFO"
-        LOGGING_PATH = f"{HOME_DIR}/forever_proc.log"
-        SETTINGS_PATH = f"{HOME_DIR}/.config/forever_proc/settings.json"
+	logger = setup_logger(settings[0], settings[1])
+	logger.debug(f"[SYSTEM_INFO]\nHOME_DIR: {HOME_DIR}\nDEBUGGING: {str(DEBUG)}\nLOG_LEVEL: {settings[0]}\nLOGGING_PATH: {settings[1]}\nSETTINGS_PATH: {settings[2]}\nMAX_JOBS: {settings[3]}")
 
-    logger = setup_logger(LOG_LEVEL, LOGGING_PATH)
-    logger.debug(f"[SYSTEM_INFO]\nHOME_DIR: {HOME_DIR}\nDEBUGGING: {str(DEBUG)}\nLOG_LEVEL: {LOG_LEVEL}\nLOGGING_PATH: {LOGGING_PATH}\nSETTINGS_PATH: {SETTINGS_PATH}\nMAX_JOBS: {MAX_JOBS}")
-
-    # Initialize the program and start it.
-    prog = Program(
-        SETTINGS_PATH, 
-        HOME_DIR,
-        MAX_JOBS
-    )
-    prog.start()
-    sys.exit(0)
+	# Initialize the program and start it.
+	prog = Program(
+		settings[2], 
+		settings[3],
+		HOME_DIR
+	)
+	prog.start()
+	sys.exit(0)
